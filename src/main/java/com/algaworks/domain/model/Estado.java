ff6e8da67e40d3fa882ec495f4d1300f18e99a69
Emmanoel <emmanoel_95@hotmail.com>
2023-10-24 17:44:02 -0300

    refactor(Estado): add validations to fields

--- src/main/java/com/algaworks/domain/model/Estado.java
+++ src/main/java/com/algaworks/domain/model/Estado.java
@@ -1,20 +1,25 @@
 package com.algaworks.domain.model;
 
+import com.algaworks.Groups;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Estado {
 
+    @NotNull(groups = Groups.EstadoId.class)
     @Id
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
