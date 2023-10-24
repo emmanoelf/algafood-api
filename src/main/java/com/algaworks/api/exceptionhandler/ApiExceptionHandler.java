c59133100cce6fe2f04a036d48597b4ba3b0fc33
Emmanoel <emmanoel_95@hotmail.com>
2023-10-24 18:46:59 -0300

    refactor(ApiExceptionHandler): add customs messages to response

--- src/main/java/com/algaworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,9 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +33,9 @@ import java.util.stream.Collectors;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     public static final String MSG_ERRO_GENERICO_USUARIO_FINAL = "Ocorreu um erro inesperado no sistema." +
             " Tente novamente e se o problema persistir, " + "entre em contato com o administrador do sistema";
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -168,12 +174,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         BindingResult bindingResult = ex.getBindingResult();
         List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-                .map(fieldError -> Problem.Field.builder()
+                .map(fieldError -> {
+                    String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+                    return Problem.Field.builder()
                         .name(fieldError.getField())
-                        .userMessage(fieldError.getDefaultMessage())
-                        .build())
+                        .userMessage(message)
+                        .build();
+                })
                 .collect(Collectors.toList());
-
 
         Problem problem = createProblemBuilder(status, problemType, detail)
                 .userMessage(detail)
