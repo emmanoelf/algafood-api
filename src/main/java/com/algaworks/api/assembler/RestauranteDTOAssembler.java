4d8c108de6227ff5b1be56291e97f02c715d3efa
Emmanoel <emmanoel_95@hotmail.com>
2023-10-31 17:01:35 -0300

    refactor(RestauranteDTOAssembler): use modelMaper

--- src/main/java/com/algaworks/api/assembler/RestauranteDTOAssembler.java
+++ src/main/java/com/algaworks/api/assembler/RestauranteDTOAssembler.java
@@ -1,8 +1,9 @@
 package com.algaworks.api.assembler;
 
-import com.algaworks.api.model.CozinhaDTO;
 import com.algaworks.api.model.RestauranteDTO;
 import com.algaworks.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,18 +11,12 @@ import java.util.stream.Collectors;
 
 @Component
 public class RestauranteDTOAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     public RestauranteDTO toDTO(Restaurante restaurante) {
-        CozinhaDTO cozinhaDTO = new CozinhaDTO();
-        cozinhaDTO.setId(restaurante.getCozinha().getId());
-        cozinhaDTO.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteDTO restauranteDTO = new RestauranteDTO();
-        restauranteDTO.setId(restaurante.getId());
-        restauranteDTO.setNome(restaurante.getNome());
-        restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteDTO.setCozinha(cozinhaDTO);
-
-        return restauranteDTO;
+        return modelMapper.map(restaurante, RestauranteDTO.class);
     }
 
     public List<RestauranteDTO> toColletionDTO(List<Restaurante> restaurantes){
