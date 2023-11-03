48934de7f26864eb05fd7e725b29d87a147b3b15
Emmanoel <emmanoel_95@hotmail.com>
2023-11-03 18:42:59 -0300

    feat(FromaPagamentoDTO): add DTO, methods to convert to domain and dto

---
+++ src/main/java/com/algaworks/api/assembler/FormaPagamentoDTOAssembler.java
@@ -0,0 +1,25 @@
+package com.algaworks.api.assembler;
+
+import com.algaworks.api.model.FormaPagamentoDTO;
+import com.algaworks.domain.model.FormaPagamento;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class FormaPagamentoDTOAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public FormaPagamentoDTO toDTO(FormaPagamento formaPagamento){
+        return modelMapper.map(formaPagamento, FormaPagamentoDTO.class);
+    }
+
+    public List<FormaPagamentoDTO> toColletionDTO(List<FormaPagamento> formasPagamentos){
+        return formasPagamentos.stream().map(this::toDTO).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/algaworks/api/model/FormaPagamentoDTO.java
@@ -0,0 +1,11 @@
+package com.algaworks.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormaPagamentoDTO {
+    private Long id;
+    private String descricao;
+}
