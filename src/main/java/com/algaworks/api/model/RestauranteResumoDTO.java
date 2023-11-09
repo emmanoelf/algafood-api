d4f0639239aaed6fe94528734b0be868c47a62fb
Emmanoel <emmanoel_95@hotmail.com>
2023-11-09 15:21:13 -0300

    feat(RestauranteResumoDTO): add dto simplificated

---
+++ src/main/java/com/algaworks/api/model/RestauranteResumoDTO.java
@@ -0,0 +1,11 @@
+package com.algaworks.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestauranteResumoDTO {
+    private Long id;
+    private String nome;
+}
