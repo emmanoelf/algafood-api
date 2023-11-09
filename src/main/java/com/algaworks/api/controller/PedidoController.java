397c24fbe8ec4d5ae6970a89081889a67662a250
Emmanoel <emmanoel_95@hotmail.com>
2023-11-09 15:23:08 -0300

    feat(PedidoController): add endpoints to list all pedidos and by id

---
+++ src/main/java/com/algaworks/api/controller/PedidoController.java
@@ -0,0 +1,39 @@
+package com.algaworks.api.controller;
+
+import com.algaworks.api.assembler.PedidoDTOAssembler;
+import com.algaworks.api.model.PedidoDTO;
+import com.algaworks.domain.model.Pedido;
+import com.algaworks.domain.repository.PedidoRepository;
+import com.algaworks.domain.service.EmissaoPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private EmissaoPedidoService emissaoPedidoService;
+
+    @Autowired
+    private PedidoDTOAssembler pedidoDTOAssembler;
+
+    @GetMapping
+    public List<PedidoDTO> listar(){
+        List<Pedido> pedidos = this.pedidoRepository.findAll();
+        return this.pedidoDTOAssembler.toCollectionDTO(pedidos);
+    }
+
+    @GetMapping("/{id}")
+    public PedidoDTO buscar(@PathVariable Long id){
+        Pedido pedido = this.emissaoPedidoService.buscarOuFalhar(id);
+        return this.pedidoDTOAssembler.toDTO(pedido);
+    }
+}
