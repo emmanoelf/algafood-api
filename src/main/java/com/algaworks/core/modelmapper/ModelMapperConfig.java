f3aa59272998f287b1c62bffd7867de9e405f7e5
Emmanoel <emmanoel_95@hotmail.com>
2023-11-10 17:30:54 -0300

    refactor(ModelMapperConfig): add type to skip map itemPedido idProduto

--- src/main/java/com/algaworks/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/core/modelmapper/ModelMapperConfig.java
@@ -1,7 +1,9 @@
 package com.algaworks.core.modelmapper;
 
 import com.algaworks.api.model.EnderecoDTO;
+import com.algaworks.api.model.input.ItemPedidoInput;
 import com.algaworks.domain.model.Endereco;
+import com.algaworks.domain.model.ItemPedido;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +17,12 @@ public class ModelMapperConfig {
         ModelMapper modelMapper = new ModelMapper();
         TypeMap<Endereco, EnderecoDTO> typeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
 
+
         typeMap.<String>addMapping(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
                 (enderecoDtoDestination, value) -> enderecoDtoDestination.getCidade().setEstado(value));
+
+        modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+                .addMappings(mapper -> mapper.skip(ItemPedido::setId));
 
         return modelMapper;
     }
