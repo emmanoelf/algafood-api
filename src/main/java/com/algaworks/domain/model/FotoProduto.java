035b4cfbfffd339970830c9daaa2340f3334b207
Emmanoel <emmanoel_95@hotmail.com>
2023-11-28 16:33:10 -0300

    refactor(CatalogoFotoProdutoService): delete fotoProduto if already exists then save a new one

--- src/main/java/com/algaworks/domain/model/FotoProduto.java
+++ src/main/java/com/algaworks/domain/model/FotoProduto.java
@@ -26,4 +26,11 @@ public class FotoProduto {
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     private Produto produto;
+
+    public Long getRestauranteId(){
+        if(this.getProduto() != null){
+            return this.getProduto().getRestaurante().getId();
+        }
+        return null;
+    }
 }
--- src/main/java/com/algaworks/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/domain/service/CatalogoFotoProdutoService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class CatalogoFotoProdutoService {
     @Autowired
@@ -13,6 +15,15 @@ public class CatalogoFotoProdutoService {
 
     @Transactional
     public FotoProduto salvar(FotoProduto foto){
+        Long restauranteId = foto.getRestauranteId();
+        Long produtoId = foto.getProduto().getId();
+
+        Optional<FotoProduto> fotoExistente = this.produtoRepository.findFotoById(restauranteId, produtoId);
+
+        if(fotoExistente.isPresent()){
+            this.produtoRepository.delete(fotoExistente.get());
+        }
+
         return produtoRepository.save(foto);
     }
 }
