a125618dcf274e785d76f0880b020fd9eb0c6659
Emmanoel <emmanoel_95@hotmail.com>
2023-11-28 16:31:22 -0300

    feat(ProdutoRepository): add method findFotoById

--- src/main/java/com/algaworks/domain/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/domain/repository/ProdutoRepository.java
@@ -1,5 +1,6 @@
 package com.algaworks.domain.repository;
 
+import com.algaworks.domain.model.FotoProduto;
 import com.algaworks.domain.model.Produto;
 import com.algaworks.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,4 +19,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>, Produto
 
     @Query("from Produto p where p.ativo = true and p.restaurante = :restaurante")
     List<Produto> findAtivosByRestaurante(Restaurante restaurante);
+
+    @Query("select f from FotoProduto f join f.produto p " +
+            "where p.restaurante.id = :restauranteId and f.produto.id = :produtoId")
+    Optional<FotoProduto> findFotoById(Long restauranteId, Long produtoId);
 }
