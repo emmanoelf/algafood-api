1dd06ad0b4b4ad789d1f7812d9186f4cf6f5dcff
Emmanoel <emmanoel_95@hotmail.com>
2023-11-28 13:23:39 -0300

    feat(RestauranteProdutoFotoController): add endpoint to add a photo to a product

---
+++ src/main/java/com/algaworks/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,28 @@
+package com.algaworks.api.controller;
+
+import com.algaworks.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid  FotoProdutoInput fotoProdutoInput){
+        var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+
+        var arquivoFoto = Path.of("path", nomeArquivo);
+
+        try{
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+}
