f1ab7e1c01d578caad6aac12f49f9beb6d0c5d0b
Emmanoel <emmanoel_95@hotmail.com>
2023-11-28 15:34:22 -0300

    feat(ProdutoRepository): add method save to FotoProduto

---
+++ src/main/java/com/algaworks/domain/repository/ProdutoRepositoryQueries.java
@@ -0,0 +1,7 @@
+package com.algaworks.domain.repository;
+
+import com.algaworks.domain.model.FotoProduto;
+
+public interface ProdutoRepositoryQueries {
+    FotoProduto save(FotoProduto foto);
+}
---
+++ src/main/java/com/algaworks/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.algaworks.infrastructure.repository;
+
+import com.algaworks.domain.model.FotoProduto;
+import com.algaworks.domain.repository.ProdutoRepositoryQueries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
+
+    @Autowired
+    private EntityManager manager;
+
+    @Transactional
+    @Override
+    public FotoProduto save(FotoProduto foto) {
+        return this.manager.merge(foto);
+    }
+}
