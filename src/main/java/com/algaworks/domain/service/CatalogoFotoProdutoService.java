69541b2d1dbf78f57058c95aa10e485f11a9ecea
Emmanoel <emmanoel_95@hotmail.com>
2023-11-29 17:17:43 -0300

    refactor(CatalogoFotoProdutoService): add builder to create a new NovaFoto in local storage

--- src/main/java/com/algaworks/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/domain/service/CatalogoFotoProdutoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 @Service
@@ -13,10 +14,14 @@ public class CatalogoFotoProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private FotoStorageService fotoStorageService;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto){
+    public FotoProduto salvar(FotoProduto foto, InputStream inputStream){
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
+        String novoNomeArquivo = this.fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
 
         Optional<FotoProduto> fotoExistente = this.produtoRepository.findFotoById(restauranteId, produtoId);
 
@@ -24,6 +29,17 @@ public class CatalogoFotoProdutoService {
             this.produtoRepository.delete(fotoExistente.get());
         }
 
-        return produtoRepository.save(foto);
+        foto.setNomeArquivo(novoNomeArquivo);
+        produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        FotoStorageService.NovaFoto novaFoto = FotoStorageService.NovaFoto.builder()
+                .nomeArquivo(foto.getNomeArquivo())
+                .inputStream(inputStream)
+                .build();
+
+        this.fotoStorageService.armazenar(novaFoto);
+
+        return foto;
     }
 }
