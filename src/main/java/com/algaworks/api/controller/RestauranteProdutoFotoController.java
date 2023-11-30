7250dfebeb0bf49b213d4bf4d080397cfae5f8eb
Emmanoel <emmanoel_95@hotmail.com>
2023-11-30 13:38:05 -0300

    feat(RestauranteProdutoFotoController): add endpoint to return content type image to client

--- src/main/java/com/algaworks/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/api/controller/RestauranteProdutoFotoController.java
@@ -3,17 +3,22 @@ package com.algaworks.api.controller;
 import com.algaworks.api.assembler.FotoProdutoDTOAssembler;
 import com.algaworks.api.model.FotoProdutoDTO;
 import com.algaworks.api.model.input.FotoProdutoInput;
+import com.algaworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.domain.model.FotoProduto;
 import com.algaworks.domain.model.Produto;
 import com.algaworks.domain.service.CadastroProdutoService;
 import com.algaworks.domain.service.CatalogoFotoProdutoService;
+import com.algaworks.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -26,6 +31,9 @@ public class RestauranteProdutoFotoController {
 
     @Autowired
     private FotoProdutoDTOAssembler fotoProdutoDTOAssembler;
+
+    @Autowired
+    private FotoStorageService fotoStorageService;
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid  FotoProdutoInput fotoProdutoInput) throws IOException {
@@ -45,9 +53,22 @@ public class RestauranteProdutoFotoController {
         return this.fotoProdutoDTOAssembler.toDTO(fotoSalva);
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoDTO buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId){
         FotoProduto fotoProduto = this.catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
         return this.fotoProdutoDTOAssembler.toDTO(fotoProduto);
+    }
+
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId){
+        try{
+            FotoProduto fotoProduto = this.catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+            InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(inputStream));
+        }catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
