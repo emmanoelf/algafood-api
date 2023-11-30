aed24c90dca292aac62e38ce0f8ce81bcfb08e2d
Emmanoel <emmanoel_95@hotmail.com>
2023-11-30 11:03:05 -0300

    feat(FotoProdutoNaoEncontradaException): add exception

---
+++ src/main/java/com/algaworks/domain/exception/FotoProdutoNaoEncontradaException.java
@@ -0,0 +1,12 @@
+package com.algaworks.domain.exception;
+
+public class FotoProdutoNaoEncontradaException extends EntidadeNaoEncontradaException{
+    public FotoProdutoNaoEncontradaException(String mensagem){
+        super(mensagem);
+    }
+
+    public FotoProdutoNaoEncontradaException(Long restauranteId, Long produtoId){
+        this(String.format("Não existe um cadastro de foto com código %d para o restaurante de código %d",
+                produtoId, restauranteId));
+    }
+}
