4aabb6c8d1989c6b9bbf1a6831b442a66067128d
Emmanoel <emmanoel_95@hotmail.com>
2024-01-30 00:27:34 -0300

    feat(S3FotoStorageService): add service thats send image and remove image to s3

---
+++ src/main/java/com/algaworks/infrastructure/service/storage/S3FotoStorageService.java
@@ -0,0 +1,68 @@
+package com.algaworks.infrastructure.service.storage;
+
+import com.algaworks.core.storage.StorageProperties;
+import com.algaworks.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class S3FotoStorageService implements FotoStorageService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    private StorageProperties storageProperties;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+        try{
+            String caminhoArquivo = this.getCaminhoArquivo(novaFoto.getNomeArquivo());
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(novaFoto.getContentType());
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    storageProperties.getS3().getBucket(),
+                    caminhoArquivo,
+                    novaFoto.getInputStream(),
+                    objectMetadata
+            ).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            amazonS3.putObject(putObjectRequest);
+        }catch (Exception e){
+               throw new StorageException("Não foi possível enviar arquivo para Amazon S3", e);
+        }
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+        try{
+            String caminhoArquivo = this.getCaminhoArquivo(nomeArquivo);
+
+            DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(
+                    storageProperties.getS3().getBucket(), caminhoArquivo
+            );
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        }catch (Exception e){
+            throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
+        }
+    }
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+
+    private String getCaminhoArquivo(String nomeArquivo){
+        return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
+    }
+}
