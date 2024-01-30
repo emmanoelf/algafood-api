2d830b043c683db7ed4295f9094ba51def348df8
Emmanoel <emmanoel_95@hotmail.com>
2024-01-30 00:28:04 -0300

    feat(RestauranteProdutoFotoController): add route to delete image from restauranteId and produtcId

--- src/main/java/com/algaworks/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/api/controller/RestauranteProdutoFotoController.java
@@ -11,6 +11,7 @@ import com.algaworks.domain.service.CatalogoFotoProdutoService;
 import com.algaworks.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -79,6 +80,12 @@ public class RestauranteProdutoFotoController {
         }catch (EntidadeNaoEncontradaException e){
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long restauranteId, @PathVariable Long produtoId){
+        this.catalogoFotoProdutoService.excluir(restauranteId, produtoId);
     }
 
     private void verificarCompatibilidade(MediaType mediaTypeFoto, List<MediaType> mediaTypesAceitas)
