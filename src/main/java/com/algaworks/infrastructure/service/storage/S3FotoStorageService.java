401699d071d6e7e0d5b77af7d616b5c940ba35bd
Emmanoel <emmanoel_95@hotmail.com>
2024-01-30 01:14:53 -0300

    refactor(S3FotoStorageService): add builder to return url

--- src/main/java/com/algaworks/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/infrastructure/service/storage/S3FotoStorageService.java
@@ -8,11 +8,9 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.io.InputStream;
+import java.net.URL;
 
-@Service
 public class S3FotoStorageService implements FotoStorageService {
 
     @Autowired
@@ -36,7 +34,7 @@ public class S3FotoStorageService implements FotoStorageService {
                     objectMetadata
             ).withCannedAcl(CannedAccessControlList.PublicRead);
 
-            amazonS3.putObject(putObjectRequest);
+            this.amazonS3.putObject(putObjectRequest);
         }catch (Exception e){
                throw new StorageException("Não foi possível enviar arquivo para Amazon S3", e);
         }
@@ -48,21 +46,25 @@ public class S3FotoStorageService implements FotoStorageService {
             String caminhoArquivo = this.getCaminhoArquivo(nomeArquivo);
 
             DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(
-                    storageProperties.getS3().getBucket(), caminhoArquivo
+                    this.storageProperties.getS3().getBucket(), caminhoArquivo
             );
 
-            amazonS3.deleteObject(deleteObjectRequest);
+            this.amazonS3.deleteObject(deleteObjectRequest);
         }catch (Exception e){
             throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
         }
     }
 
     @Override
-    public InputStream recuperar(String nomeArquivo) {
-        return null;
+    public FotoRecuperada recuperar(String nomeArquivo) {
+        String caminhoArquivo = this.getCaminhoArquivo(nomeArquivo);
+
+        URL url = this.amazonS3.getUrl(this.storageProperties.getS3().getBucket(), caminhoArquivo);
+
+        return FotoRecuperada.builder().url(url.toString()).build();
     }
 
     private String getCaminhoArquivo(String nomeArquivo){
-        return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
+        return String.format("%s/%s", this.storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
     }
 }
