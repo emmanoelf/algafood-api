bf5eae1aa1ee33313165d19420d42c47e405b8d7
Emmanoel <emmanoel_95@hotmail.com>
2024-01-31 23:58:58 -0300

    refactor(SmtpEnvioEmmailService): add private method to process template before send

--- src/main/java/com/algaworks/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -2,10 +2,13 @@ package com.algaworks.infrastructure.service.email;
 
 import com.algaworks.core.email.EmailProperties;
 import com.algaworks.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.internet.MimeMessage;
 
@@ -19,15 +22,19 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     @Autowired
     private EmailProperties emailProperties;
 
+    @Autowired
+    private Configuration freemarkerConfig;
+
     @Override
     public void enviar(Mensagem mensagem) {
         try{
+            String corpo = processarTemplate(mensagem);
             MimeMessage mimeMessage = mailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
             helper.setSubject(mensagem.getAssunto());
-            helper.setText(mensagem.getCorpo(), true);
+            helper.setText(corpo, true);
             helper.setFrom(emailProperties.getRemetente());
 
 
@@ -35,6 +42,15 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
         }catch (Exception e){
             throw new EmailException("Não foi possível enviar email", e);
         }
+    }
 
+    private String processarTemplate(Mensagem mensagem){
+        try{
+            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
+        }catch (Exception e){
+            throw new EmailException("Não foi possível montar o template do e-mail", e);
+        }
     }
 }
