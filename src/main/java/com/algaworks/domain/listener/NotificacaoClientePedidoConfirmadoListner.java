2ea75674c28bb0197a4ad46fce363e5869074e17
Emmanoel <emmanoel_95@hotmail.com>
2024-02-09 22:58:14 -0300

    feat(NotificacaoClientePedidoConfirmadoListener): add send email when pedido was confirmed

---
+++ src/main/java/com/algaworks/domain/listener/NotificacaoClientePedidoConfirmadoListner.java
@@ -0,0 +1,30 @@
+package com.algaworks.domain.listener;
+
+import com.algaworks.domain.event.PedidoConfirmadoEvent;
+import com.algaworks.domain.model.Pedido;
+import com.algaworks.domain.service.EnvioEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificacaoClientePedidoConfirmadoListner {
+
+    @Autowired
+    private EnvioEmailService envioEmailService;
+
+    @EventListener
+    public void aoConfimarPedido(PedidoConfirmadoEvent event){
+        Pedido pedido = event.getPedido();
+
+        EnvioEmailService.Mensagem mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("pedido-confirmado.html")
+                .variavel("pedido", pedido)
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmailService.enviar(mensagem);
+    }
+
+}
