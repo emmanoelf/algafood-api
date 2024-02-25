00704fe29142ccfe89f6f774890572806f0be5cc
Emmanoel <emmanoel_95@hotmail.com>
2024-02-25 17:42:29 -0300

    feat(FormaPagamentoRepository): add getDataUltimaAtualizacao method

--- src/main/java/com/algaworks/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/domain/repository/FormaPagamentoRepository.java
@@ -2,11 +2,16 @@ package com.algaworks.domain.repository;
 
 import com.algaworks.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
 public interface FormaPagamentoRepository  extends JpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    OffsetDateTime getDataUltimaAtualizacao();
 
 }
