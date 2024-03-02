8a1b992cf7ebf0542c17416e00aefb14d5061c45
Emmanoel <emmanoel_95@hotmail.com>
2024-03-02 18:38:46 -0300

    docs(PageableModelOpenApi): add pageable

--- src/main/java/com/algaworks/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/core/openapi/SpringFoxConfig.java
@@ -1,10 +1,12 @@
 package com.algaworks.core.openapi;
 
 import com.algaworks.api.exceptionhandler.Problem;
+import com.algaworks.core.openapi.model.PageableModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -46,6 +48,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
                 .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(Problem.class))
+                .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"))
                 .tags(new Tag("Grupos", "Gerencia os grupos"));
