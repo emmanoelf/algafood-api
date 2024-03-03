c1921d072d75c8837d164f9751a44a1e5c25b3be
Emmanoel <emmanoel_95@hotmail.com>
2024-03-03 15:29:19 -0300

    docs(GrupoPermissaoControllerOpenApi): add to swagger

--- src/main/java/com/algaworks/api/controller/GrupoPermissaoController.java
+++ src/main/java/com/algaworks/api/controller/GrupoPermissaoController.java
@@ -2,18 +2,20 @@ package com.algaworks.api.controller;
 
 import com.algaworks.api.assembler.PermissaoDTOAssembler;
 import com.algaworks.api.model.PermissaoDTO;
+import com.algaworks.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.algaworks.domain.model.Grupo;
 import com.algaworks.domain.model.Permissao;
 import com.algaworks.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+@RequestMapping(path = "/grupos/{grupoId}/permissoes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
     @Autowired
     private CadastroGrupoService cadastroGrupoService;
 
