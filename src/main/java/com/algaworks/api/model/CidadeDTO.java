c6e1130cda69b7cad1f81be012d6281a44e52e41
Emmanoel <emmanoel_95@hotmail.com>
2024-03-04 22:42:32 -0300

    refactor(CidadeDTO/EstadoDTO): add representation model from hateoas

--- src/main/java/com/algaworks/api/model/CidadeDTO.java
+++ src/main/java/com/algaworks/api/model/CidadeDTO.java
@@ -2,10 +2,13 @@ package com.algaworks.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
+@Relation(collectionRelation = "cidades")
 @Getter
 @Setter
-public class CidadeDTO {
+public class CidadeDTO extends RepresentationModel<CidadeDTO> {
     private Long id;
     private String nome;
     private EstadoDTO estado;
--- src/main/java/com/algaworks/api/model/EstadoDTO.java
+++ src/main/java/com/algaworks/api/model/EstadoDTO.java
@@ -2,10 +2,11 @@ package com.algaworks.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
-public class EstadoDTO {
+public class EstadoDTO extends RepresentationModel<EstadoDTO> {
     private Long id;
     private String nome;
 }
