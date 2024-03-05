8af00e32f446ce365ea20fd6b035b533df872769
Emmanoel <emmanoel_95@hotmail.com>
2024-03-05 16:06:50 -0300

    refactor(UsuarioDTO): add representation to use hateoas

--- src/main/java/com/algaworks/api/assembler/UsuarioDTOAssembler.java
+++ src/main/java/com/algaworks/api/assembler/UsuarioDTOAssembler.java
@@ -1,9 +1,14 @@
 package com.algaworks.api.assembler;
 
+import com.algaworks.api.controller.UsuarioController;
+import com.algaworks.api.controller.UsuarioGrupoController;
 import com.algaworks.api.model.UsuarioDTO;
 import com.algaworks.domain.model.Usuario;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -11,16 +16,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class UsuarioDTOAssembler {
+public class UsuarioDTOAssembler extends RepresentationModelAssemblerSupport<Usuario, UsuarioDTO> {
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public UsuarioDTO toDTO(Usuario usuario){
-        return this.modelMapper.map(usuario, UsuarioDTO.class);
+    public UsuarioDTOAssembler(){
+        super(UsuarioController.class, UsuarioDTO.class);
     }
 
-    public List<UsuarioDTO> toCollectionDTO(Collection<Usuario> usuarios){
-        return usuarios.stream().map(this::toDTO).collect(Collectors.toList());
+    public UsuarioDTO toModel(Usuario usuario){
+        UsuarioDTO usuarioDto = createModelWithId(usuario.getId(), usuario);
+        this.modelMapper.map(usuario, usuarioDto);
+
+        usuarioDto.add(WebMvcLinkBuilder
+                .linkTo(UsuarioController.class).withSelfRel());
+
+        usuarioDto.add(WebMvcLinkBuilder
+                .linkTo(WebMvcLinkBuilder.methodOn(UsuarioGrupoController.class).listar(usuario.getId())).withRel("usuarios"));
+
+        return usuarioDto;
+    }
+
+    @Override
+    public CollectionModel<UsuarioDTO> toCollectionModel(Iterable<? extends Usuario> entities) {
+        return super.toCollectionModel(entities)
+                .add(WebMvcLinkBuilder.linkTo(UsuarioController.class).withSelfRel());
     }
 }
