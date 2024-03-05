ef0adf677c27e0f6345a399fd4888ebd5d92f660
Emmanoel <emmanoel_95@hotmail.com>
2024-03-05 17:44:43 -0300

    refactor(EstadoDTO/Assembler): change representation to hal

--- src/main/java/com/algaworks/api/assembler/EstadoDTOAssembler.java
+++ src/main/java/com/algaworks/api/assembler/EstadoDTOAssembler.java
@@ -1,25 +1,39 @@
 package com.algaworks.api.assembler;
 
+import com.algaworks.api.controller.EstadoController;
 import com.algaworks.api.model.EstadoDTO;
 import com.algaworks.domain.model.Estado;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class EstadoDTOAssembler {
+public class EstadoDTOAssembler extends RepresentationModelAssemblerSupport<Estado, EstadoDTO> {
     @Autowired
     private ModelMapper modelMapper;
 
-    public EstadoDTO toDTO(Estado estado){
-        return modelMapper.map(estado, EstadoDTO.class);
+    public EstadoDTOAssembler(){
+        super(EstadoController.class, EstadoDTO.class);
     }
 
-    public List<EstadoDTO> toCollectionDTO(List<Estado> estados){
-        return estados.stream()
-                .map(estado -> toDTO(estado)).collect(Collectors.toList());
+    public EstadoDTO toModel(Estado estado){
+        EstadoDTO estadoDto = createModelWithId(estado.getId(), estado);
+
+        modelMapper.map(estado, estadoDto);
+
+        estadoDto.add(WebMvcLinkBuilder.linkTo(EstadoController.class).withSelfRel());
+
+        return estadoDto;
+    }
+
+    @Override
+    public CollectionModel<EstadoDTO> toCollectionModel(Iterable<? extends Estado> entities) {
+        return super.toCollectionModel(entities).add(WebMvcLinkBuilder.linkTo(EstadoController.class).withSelfRel());
     }
 }
--- src/main/java/com/algaworks/api/model/EnderecoDTO.java
+++ src/main/java/com/algaworks/api/model/EnderecoDTO.java
@@ -3,10 +3,13 @@ package com.algaworks.api.model;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
+@Relation(collectionRelation = "estados")
 @Getter
 @Setter
-public class EnderecoDTO {
+public class EnderecoDTO extends RepresentationModel<EnderecoDTO> {
 
     @ApiModelProperty(example = "12345-666")
     private String cep;
