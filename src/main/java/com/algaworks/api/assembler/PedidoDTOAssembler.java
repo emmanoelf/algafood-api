c49b2533bb45993269bd56dc00be3aa4d8eb307b
Emmanoel <emmanoel_95@hotmail.com>
2024-03-07 18:25:38 -0300

    refactor(PedidoDtoAssembler): add filterVariables to template link

--- src/main/java/com/algaworks/api/assembler/PedidoDTOAssembler.java
+++ src/main/java/com/algaworks/api/assembler/PedidoDTOAssembler.java
@@ -5,6 +5,10 @@ import com.algaworks.api.model.PedidoDTO;
 import com.algaworks.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
@@ -20,11 +24,26 @@ public class PedidoDTOAssembler extends RepresentationModelAssemblerSupport<Pedi
 
     @Override
     public PedidoDTO toModel(Pedido pedido){
-        PedidoDTO pedidoDto = createModelWithId(pedido.getId(), pedido);
+        PedidoDTO pedidoDto = createModelWithId(pedido.getCodigo(), pedido);
 
         this.modelMapper.map(pedido, pedidoDto);
 
-        pedidoDto.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
+        );
+
+        TemplateVariables filterVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", TemplateVariable.VariableType.REQUEST_PARAM)
+        );
+
+        String pedidosUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
+
+        pedidoDto.add(new Link(UriTemplate.of(pedidosUrl, pageVariables.concat(filterVariables)), "pedidos"));
 
         pedidoDto.getRestaurante().add(WebMvcLinkBuilder.linkTo
                 (WebMvcLinkBuilder.methodOn(RestauranteController.class)
