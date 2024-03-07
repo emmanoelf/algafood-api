097aad15aed093ce9a48cd88f8b6744efef57e2b
Emmanoel <emmanoel_95@hotmail.com>
2024-03-07 11:56:43 -0300

    refactor(PedidoResumoDTOAssembler): remove toCollection, change to representation hal

--- src/main/java/com/algaworks/api/assembler/PedidoResumoDTOAssembler.java
+++ src/main/java/com/algaworks/api/assembler/PedidoResumoDTOAssembler.java
@@ -1,24 +1,41 @@
 package com.algaworks.api.assembler;
 
+import com.algaworks.api.controller.PedidoController;
+import com.algaworks.api.controller.RestauranteController;
+import com.algaworks.api.controller.UsuarioController;
 import com.algaworks.api.model.PedidoResumoDTO;
 import com.algaworks.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
-public class PedidoResumoDTOAssembler {
+public class PedidoResumoDTOAssembler extends RepresentationModelAssemblerSupport<Pedido, PedidoResumoDTO> {
     @Autowired
     private ModelMapper modelMapper;
 
-    public PedidoResumoDTO toDTO(Pedido pedido){
-        return this.modelMapper.map(pedido, PedidoResumoDTO.class);
+    public PedidoResumoDTOAssembler(){
+        super(PedidoController.class, PedidoResumoDTO.class);
     }
 
-    public List<PedidoResumoDTO> toCollectionDTO(List<Pedido> pedidos){
-        return pedidos.stream().map(this::toDTO).collect(Collectors.toList());
+    @Override
+    public PedidoResumoDTO toModel(Pedido pedido){
+        PedidoResumoDTO pedidoResumoDto = createModelWithId(pedido.getId(), pedido);
+
+        this.modelMapper.map(pedido, pedidoResumoDto);
+
+        pedidoResumoDto.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+
+        pedidoResumoDto.getRestaurante().add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(RestauranteController.class)
+                        .buscar(pedido.getRestaurante().getId())).withSelfRel());
+
+        pedidoResumoDto.getCliente().add(WebMvcLinkBuilder.linkTo
+                (WebMvcLinkBuilder.methodOn(UsuarioController.class)
+                        .buscar(pedido.getCliente().getId())).withSelfRel());
+
+        return pedidoResumoDto;
     }
 }
